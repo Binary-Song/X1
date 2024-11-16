26cc1a1a325f68bdc792df89fe8b4942fe11eebe
yangerhang <yeh189@126.com>
2024-11-16 21:39:42 +0800

    fix assert

--- Source/X1/X1Assert.cpp
+++ Source/X1/X1Assert.cpp
@@ -5,49 +5,63 @@
 #include <map>
 #include <string>
 DEFINE_LOG_CATEGORY(LogAssert);
-thread_local std::map<FString, int> codes;
 thread_local int nextId = 0;
 
 void _X1AssertionHelper::X1AssertionFailed_WithCustomLog(
     const char *file, int line, const char *cond, const FString &userMessage,
     const std::function<void(const FString &)> &logFunc)
 {
-    FString src;
-    src.Reserve(strlen(file) + 6);
-    src += file;
-    src += ":";
-    src += FString::FromInt(line);
-
-    int id{};
-    const bool isNew = !codes.contains(src);
-    if (isNew)
-        codes[src] = nextId++;
-    id = nextId - 1;
-
-    FString message;
-    message.Reserve(userMessage.Len() + strlen(file) + 30);
-    message.Append("Assert! Msg=");
-    message.Append(userMessage);
-    message.Append(", Cond=");
-    message.Append(cond);
-    message.Append(", Src=");
-    message.Append(src);
-
-    if (GEngine)
-        GEngine->AddOnScreenDebugMessage(id, 20.f, FColor::Red, message);
-    if (isNew)
-    {
-        logFunc(message);
-    }
 }
 
 void _X1AssertionHelper::X1AssertionFailed(const char *file, int line,
                                            const char *cond,
-                                           const FString &message)
+                                           const FString &userMessage)
 {
+    FString message;
+    message.Reserve(userMessage.Len() + strlen(file) + 30);
+    message.Append("Assert! ");
+    if (userMessage.Len() > 0)
+    {
+        message.Append("Msg=");
+        message.Append(userMessage);
+        message.Append(", ");
+    }
+    message.Append("Cond=");
+    message.Append(cond);
+    message.Append(", Src=");
+    message += file;
+    message += ":";
+    message += FString::FromInt(line);
+    _X1AssertionHelper::LogToScreenAndConsole(message);
+}
 
-    _X1AssertionHelper::X1AssertionFailed_WithCustomLog(
-        file, line, cond, message, [&](const FString &message) {
-            UE_LOG(LogAssert, Error, TEXT("%s"), *message);
-        });
+struct MessageDeduplicate
+{
+    int getId(const FString &msg, bool &bNew)
+    {
+        auto it = m_map.find(msg);
+        bNew = it == m_map.end();
+        if (bNew)
+        {
+            m_map[msg] = m_id;
+            m_id++;
+            return m_id - 1;
+        }
+        return it->second;
+    }
+
+private:
+    int m_id = 1;
+    std::map<FString, int> m_map;
+};
+
+void _X1AssertionHelper::LogToScreenAndConsole(const FString &message)
+{
+    thread_local MessageDeduplicate md;
+    bool bNew = false;
+    if (GEngine)
+        GEngine->AddOnScreenDebugMessage(md.getId(message, bNew), 20.f,
+                                         FColor::Red, message);
+    if (bNew)
+        UE_LOG(LogAssert, Error, TEXT("%s"), *message);
 }
--- Source/X1/X1Assert.h
+++ Source/X1/X1Assert.h
@@ -10,11 +10,8 @@ class _X1AssertionHelper
 {
 public:
     static void X1AssertionFailed(const char *file, int line, const char *cond,
-                                  const FString &message);
-    static void X1AssertionFailed_WithCustomLog(
-        const char *file, int line, const char *cond, const FString &message,
-        const std::function<void(const FString &)> &logFunc);
-
+                                  const FString &message = {});
+     
     template <typename... T>
     static FString BuildFormattedString(const FString::ElementType *fmt,
                                         T &&...args)
@@ -22,18 +19,25 @@ public:
         TArray<FStringFormatArg> a = {FStringFormatArg(args)...};
         return FString::Format(fmt, a);
     }
+
+    static void LogToScreenAndConsole(const FString &message);
+
+private:
+    static void X1AssertionFailed_WithCustomLog(
+        const char *file, int line, const char *cond, const FString &message,
+        const std::function<void(const FString &)> &logFunc);
 };
 
-#define _X1_ASSERT_RET_IMPL(cond, ret, msg)                                    \
+#define _X1_ASSERT_NO_FMT(cond, ret, msg)                                      \
     {                                                                          \
         if (!(cond))                                                           \
         {                                                                      \
-            _X1AssertionHelper::X1AssertionFailed(__FILE__, __LINE__, #cond,   \
-                                                  msg);                        \
+            _X1AssertionHelper::X1AssertionFailed(__FILE__, __LINE__, #cond );                        \
             ret;                                                               \
         }                                                                      \
     }
-#define _X1_ASSERT_RET_IMPL2(cond, ret, fmt, ...)                              \
+
+#define _X1_ASSERT_FMT(cond, ret, fmt, ...)                                    \
     {                                                                          \
         if (!(cond))                                                           \
         {                                                                      \
@@ -41,28 +45,28 @@ public:
                 __FILE__, __LINE__, #cond,                                     \
                 _X1AssertionHelper::BuildFormattedString(TEXT(fmt),            \
                                                          __VA_ARGS__));        \
+            ret;                                                               \
         }                                                                      \
     }
 
+#define _X1_ASSERT_GET_OVLD(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, NAME, ...) \
+    NAME
+#define X1_ASSERT(cond, ret, ...)                                              \
+    _X1_ASSERT_GET_OVLD(__VA_ARGS__, _X1_ASSERT_FMT, _X1_ASSERT_FMT,           \
+                        _X1_ASSERT_FMT, _X1_ASSERT_FMT, _X1_ASSERT_FMT,        \
+                        _X1_ASSERT_FMT, _X1_ASSERT_FMT, _X1_ASSERT_FMT,        \
+                        _X1_ASSERT_FMT, _X1_ASSERT_NO_FMT)                     \
+    (cond, ret, __VA_ARGS__)
+
 // clang-format off
 
-// 当条件不满足时，在屏幕和Log输出消息，并从当前函数返回（不会导致程序崩溃）。
-// ASSERT: 不指定消息
-// ASSERTF: 使用 FString::Printf 格式化消息，使用C风格格式串
-// ASSERTX: 使用 FString::Format 指定消息，使用 `{}` 风格格式串
-// RET_VOID: 条件不满足时返回 void
-// RET_EMPTY: 条件不满足时返回 {}
-// RET_CONTINUE: 条件不满足时继续下个循环 （continue）
-#define X1_ASSERTF_RET_VOID(cond, fmt, ...) _X1_ASSERT_RET_IMPL(cond,  return     , FString::Printf(TEXT(fmt), __VA_ARGS__))
-#define X1_ASSERTF_RET_EMPTY(cond, fmt, ...) _X1_ASSERT_RET_IMPL(cond, return {}  , FString::Printf(TEXT(fmt), __VA_ARGS__))
-#define X1_ASSERTF_CONTINUE(cond, fmt, ...) _X1_ASSERT_RET_IMPL(cond,  continue   , FString::Printf(TEXT(fmt), __VA_ARGS__))
-#define X1_ASSERTF_IGNORE(cond, fmt, ...) _X1_ASSERT_RET_IMPL(cond,    ;          , FString::Printf(TEXT(fmt), __VA_ARGS__))
-#define X1_ASSERT_RET_VOID(cond) _X1_ASSERT_RET_IMPL(cond, return, {})
-#define X1_ASSERT_RET_EMPTY(cond) _X1_ASSERT_RET_IMPL(cond, return {}, {})
-#define X1_ASSERT_CONTINUE(cond) _X1_ASSERT_RET_IMPL(cond, continue, {})
-#define X1_ASSERT_IGNORE(cond) _X1_ASSERT_RET_IMPL(cond, ;, {})
-#define X1_ASSERTX_RET_VOID(cond, fmt, ...) _X1_ASSERT_RET_IMPL2(cond, return, fmt, __VA_ARGS__)
-#define X1_ASSERTX_RET_EMPTY(cond, fmt, ...) _X1_ASSERT_RET_IMPL2(cond, return {}, fmt, __VA_ARGS__)
-#define X1_ASSERTX_CONTINUE(cond, fmt, ...) _X1_ASSERT_RET_IMPL2(cond, continue, fmt, __VA_ARGS__)
-#define X1_ASSERTX_IGNORE(cond, fmt, ...) _X1_ASSERT_RET_IMPL2(cond, ;, fmt, __VA_ARGS__)
+// 当条件不满足时，在屏幕和Log输出消息，并从当前函数返回 void（不会导致程序崩溃）。
+#define X1_ASSERT_RET_VOID(cond,  ...) X1_ASSERT(cond, return, __VA_ARGS__)
+// 当条件不满足时，在屏幕和Log输出消息，并从当前函数返回 {}（不会导致程序崩溃）。
+#define X1_ASSERT_RET_EMPTY(cond,  ...) X1_ASSERT(cond, return {}, __VA_ARGS__)
+// 当条件不满足时，在屏幕和Log输出消息，并 continue （不会导致程序崩溃）。
+#define X1_ASSERT_CONTINUE(cond,  ...) X1_ASSERT(cond, continue, __VA_ARGS__)
+// 当条件不满足时，在屏幕和Log输出消息，并继续执行（不会导致程序崩溃）。
+#define X1_ASSERT_PASS(cond,  ...) X1_ASSERT(cond, ;, __VA_ARGS__)
+
 // clang-format on
