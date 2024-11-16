65b163a8ec86368937e5de7962e554a825dbe6a6
yangerhang <yeh189@126.com>
2024-11-16 22:51:22 +0800

    make asserts great again

--- Source/X1/X1Assert.cpp
+++ Source/X1/X1Assert.cpp
@@ -7,32 +7,35 @@
 DEFINE_LOG_CATEGORY(LogAssert);
 thread_local int nextId = 0;
 
-void _X1AssertionHelper::X1AssertionFailed_WithCustomLog(
-    const char *file, int line, const char *cond, const FString &userMessage,
-    const std::function<void(const FString &)> &logFunc)
+void _X1AssertionHelper::Log(X1LogExtras type, const char *file, int line,
+                             const char *cond, const FString &userMessage)
 {
-}
-
-void _X1AssertionHelper::X1AssertionFailed(const char *file, int line,
-                                           const char *cond,
-                                           const FString &userMessage)
-{
-    FString message;
-    message.Reserve(userMessage.Len() + strlen(file) + 30);
-    message.Append("Assert! ");
-    if (userMessage.Len() > 0)
-    {
-        message.Append("Msg=");
-        message.Append(userMessage);
-        message.Append(", ");
-    }
-    message.Append("Cond=");
-    message.Append(cond);
-    message.Append(", Src=");
-    message += file;
-    message += ":";
-    message += FString::FromInt(line);
-    _X1AssertionHelper::LogToScreenAndConsole(message);
+    const FString message = [&]() {
+        FString message; 
+        message.Reserve(userMessage.Len() + strlen(file) + 30);
+        if (type.type == X1LogType::Assert)
+            message.Append("Assert! ");
+        else
+            message.Append("Log ");
+        if (userMessage.Len() > 0)
+        {
+            message.Append("Msg=");
+            message.Append(userMessage);
+            message.Append(", ");
+        }
+        if (type.type == X1LogType::Assert)
+        {
+            message.Append("Cond=");
+            message.Append(cond);
+            message.Append(", ");
+        }
+        message.Append("Src=");
+        message.Append(file);
+        message.Append(":");
+        message.Append(FString::FromInt(line));
+        return message;
+    }();
+    _X1AssertionHelper::LogToScreenAndConsole(message, type.bRepeat);
 }
 
 struct MessageDeduplicate
@@ -55,13 +58,20 @@ private:
     std::map<FString, int> m_map;
 };
 
-void _X1AssertionHelper::LogToScreenAndConsole(const FString &message)
+void _X1AssertionHelper::LogToScreenAndConsole(const FString &message,
+                                               bool bRepeatable)
 {
     thread_local MessageDeduplicate md;
     bool bNew = false;
+    int id = md.getId(message, bNew);
+    if (bRepeatable)
+    {
+        id = -1;
+        bNew = true;
+    }
+
     if (GEngine)
-        GEngine->AddOnScreenDebugMessage(md.getId(message, bNew), 20.f,
-                                         FColor::Red, message);
+        GEngine->AddOnScreenDebugMessage(id, 20.f, FColor::Red, message);
     if (bNew)
         UE_LOG(LogAssert, Error, TEXT("%s"), *message);
 }
--- Source/X1/X1Assert.h
+++ Source/X1/X1Assert.h
@@ -5,13 +5,25 @@
 #include <functional>
 DECLARE_LOG_CATEGORY_EXTERN(LogAssert, Log, All);
 
+enum class X1LogType
+{
+    Assert,
+    Log,
+};
+
+struct X1LogExtras
+{
+    X1LogType type = {};
+    bool bRepeat = false;
+};
+
 // use assertion macros instead
 class _X1AssertionHelper
 {
 public:
-    static void X1AssertionFailed(const char *file, int line, const char *cond,
-                                  const FString &message = {});
-     
+    static void Log(X1LogExtras type, const char *file, int line,
+                    const char *cond, const FString &userMessage = {});
+
     template <typename... T>
     static FString BuildFormattedString(const FString::ElementType *fmt,
                                         T &&...args)
@@ -20,53 +32,87 @@ public:
         return FString::Format(fmt, a);
     }
 
-    static void LogToScreenAndConsole(const FString &message);
-
-private:
-    static void X1AssertionFailed_WithCustomLog(
-        const char *file, int line, const char *cond, const FString &message,
-        const std::function<void(const FString &)> &logFunc);
+    static void LogToScreenAndConsole(const FString &message,
+                                      bool repeatable = false);
 };
 
-#define _X1_ASSERT_NO_FMT(cond, ret, msg)                                      \
+#define _X1_ASSERT_FMT0(type, ret, cond)                                       \
     {                                                                          \
         if (!(cond))                                                           \
         {                                                                      \
-            _X1AssertionHelper::X1AssertionFailed(__FILE__, __LINE__, #cond );                        \
+            _X1AssertionHelper::Log(type, __FILE__, __LINE__, #cond);          \
+            ret;                                                               \
+        }                                                                      \
+    }
+#define _X1_ASSERT_FMT1(type, ret, cond, msg)                                  \
+    {                                                                          \
+        if (!(cond))                                                           \
+        {                                                                      \
+            _X1AssertionHelper::Log(type, __FILE__, __LINE__, #cond,           \
+                                    TEXT(msg));                                \
+            ret;                                                               \
+        }                                                                      \
+    }
+#define _X1_ASSERT_FMT2(type, ret, cond, fmt, ...)                             \
+    {                                                                          \
+        if (!(cond))                                                           \
+        {                                                                      \
+            _X1AssertionHelper::Log(type, __FILE__, __LINE__, #cond,           \
+                                    _X1AssertionHelper::BuildFormattedString(  \
+                                        TEXT(fmt), __VA_ARGS__));              \
             ret;                                                               \
         }                                                                      \
     }
 
-#define _X1_ASSERT_FMT(cond, ret, fmt, ...)                                    \
-    {                                                                          \
-        if (!(cond))                                                           \
-        {                                                                      \
-            _X1AssertionHelper::X1AssertionFailed(                             \
-                __FILE__, __LINE__, #cond,                                     \
-                _X1AssertionHelper::BuildFormattedString(TEXT(fmt),            \
-                                                         __VA_ARGS__));        \
-            ret;                                                               \
-        }                                                                      \
-    }
-
-#define _X1_ASSERT_GET_OVLD(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, NAME, ...) \
+#define _X1_ASSERT_GET_OVLD(_1, _2, _3, NAME, ...) \
     NAME
-#define X1_ASSERT(cond, ret, ...)                                              \
-    _X1_ASSERT_GET_OVLD(__VA_ARGS__, _X1_ASSERT_FMT, _X1_ASSERT_FMT,           \
-                        _X1_ASSERT_FMT, _X1_ASSERT_FMT, _X1_ASSERT_FMT,        \
-                        _X1_ASSERT_FMT, _X1_ASSERT_FMT, _X1_ASSERT_FMT,        \
-                        _X1_ASSERT_FMT, _X1_ASSERT_NO_FMT)                     \
-    (cond, ret, __VA_ARGS__)
+#define X1_ASSERT(type, ret, ...)                                        \
+    _X1_ASSERT_GET_OVLD(__VA_ARGS__, _X1_ASSERT_FMT2, _X1_ASSERT_FMT1,         \
+                        _X1_ASSERT_FMT0)                                       \
+    (type, ret, __VA_ARGS__)
 
 // clang-format off
+#define X1_LOGTYPE_AS X1LogExtras(X1LogType::Assert)
+#define X1_LOGTYPE_LG X1LogExtras(X1LogType::Log)
+#define X1_LOGTYPE_LR X1LogExtras(X1LogType::Log, true)
+#define X1_FAILACT_RV return
+#define X1_FAILACT_RE return {}
+#define X1_FAILACT_CO continue
+#define X1_FAILACT_PA ;
+
 
 // 当条件不满足时，在屏幕和Log输出消息，并从当前函数返回 void（不会导致程序崩溃）。
-#define X1_ASSERT_RET_VOID(cond,  ...) X1_ASSERT(cond, return, __VA_ARGS__)
+// 示例语法：
+// (1) (Foo>0)
+// (2) (Foo>0, "Foo must be greater than 0!")
+// (3) (Foo>0, "Foo={0}. Foo must be greater than 0!", Foo)
+#define X1_ASSERT_RET_VOID(...)     X1_ASSERT(X1_LOGTYPE_AS, X1_FAILACT_RV, __VA_ARGS__)
 // 当条件不满足时，在屏幕和Log输出消息，并从当前函数返回 {}（不会导致程序崩溃）。
-#define X1_ASSERT_RET_EMPTY(cond,  ...) X1_ASSERT(cond, return {}, __VA_ARGS__)
+// 示例语法：
+// (1) (Foo>0)
+// (2) (Foo>0, "Foo must be greater than 0!")
+// (3) (Foo>0, "Foo={0}. Foo must be greater than 0!", Foo)
+#define X1_ASSERT_RET_EMPTY(...)    X1_ASSERT(X1_LOGTYPE_AS, X1_FAILACT_RE, __VA_ARGS__)
 // 当条件不满足时，在屏幕和Log输出消息，并 continue （不会导致程序崩溃）。
-#define X1_ASSERT_CONTINUE(cond,  ...) X1_ASSERT(cond, continue, __VA_ARGS__)
+// 示例语法：
+// (1) (Foo>0)
+// (2) (Foo>0, "Foo must be greater than 0!")
+// (3) (Foo>0, "Foo={0}. Foo must be greater than 0!", Foo)
+#define X1_ASSERT_CONTINUE(...)     X1_ASSERT(X1_LOGTYPE_AS, X1_FAILACT_CO, __VA_ARGS__)
 // 当条件不满足时，在屏幕和Log输出消息，并继续执行（不会导致程序崩溃）。
-#define X1_ASSERT_PASS(cond,  ...) X1_ASSERT(cond, ;, __VA_ARGS__)
-
+// 示例语法：
+// (1) (Foo>0)
+// (2) (Foo>0, "Foo must be greater than 0!")
+// (3) (Foo>0, "Foo={0}. Foo must be greater than 0!", Foo)
+#define X1_ASSERT_PASS(...)         X1_ASSERT(X1_LOGTYPE_AS, X1_FAILACT_PA, __VA_ARGS__)
+// 在屏幕和Log输出消息。重复消息不会显示。
+// 示例语法：
+// (1) ("Hello world!")
+// (2) ("Hello world! Foo={0}!", Foo)
+#define X1_LOG(...)            X1_ASSERT(X1_LOGTYPE_LG, X1_FAILACT_PA, 0, __VA_ARGS__)
+// 在屏幕和Log输出消息。重复消息会显示。
+// 示例语法：
+// (1) ("Hello world!")
+// (2) ("Hello world! Foo={0}!", Foo)
+#define X1_LOG_REPEAT(...)     X1_ASSERT(X1_LOGTYPE_LR, X1_FAILACT_PA, 0, __VA_ARGS__)
 // clang-format on
